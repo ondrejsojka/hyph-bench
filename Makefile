4da8560fc2a9b5febc6de3fa0d28a2a93c51e8fd
tondach01 <ondra.metelka@seznam.cz>
2025-11-14 09:43:29 +0100

    Add dataset disambiguation

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 # languages provided in Wiktionary dump
 WIKT_LANGS = cs de el es it ms nl pl pt ru tr
+
 # non-Wiktionary datasets
-OTHER_DATASETS = cs/cshyphen_cstenten cs/cshyphen_ujc cssk/cshyphen is/hyphenation-is th/orchid de/wortliste
+# cssk/cshyphen is special as it is weighted
+CSSK = cssk/cshyphen
+OTHER_DATASETS = cs/cshyphen_cstenten cs/cshyphen_ujc is/hyphenation-is th/orchid de/wortliste
 
 # get statistics of all datasets
 stats_all_datasets: #process_wikt prepare_other
@@ -10,26 +13,38 @@ stats_all_datasets: #process_wikt prepare_other
 
 # parse Wiktionary dumps into wordlists
 process_wikt: prepare_wikt
-	$(foreach d,$(wildcard data/*/wiktionary/*.wlh),rm -f $(d);)
-	$(foreach l,$(WIKT_LANGS),python ./scripts/process_dump.py --lang $(l);)
+	@$(foreach d,$(wildcard data/*/wiktionary/*.wlh),rm -f $(d);)
+	@$(foreach l,$(WIKT_LANGS),python ./scripts/process_dump.py --lang $(l);)
 
 # create translate files for all available datasets
 translate_all: translate_wikt translate_other
 
 # create translate files for Wiktionary wordlists (which are created at first)
-translate_wikt: process_wikt
-	$(foreach l,$(wildcard data/*/wiktionary/*.wlh.tra),rm -f $(l);)
-	$(foreach l,$(wildcard data/*/wiktionary/*.wlh),python ./scripts/make_tr.py $(l);)
+translate_wikt: disambiguate_wikt
+	@$(foreach l,$(wildcard data/*/wiktionary/*.tra),rm -f $(l);)
+	@$(foreach l,$(wildcard data/*/wiktionary/*_dis.wlh),python ./scripts/make_tr.py $(l);)
 
 # create translate files for non-Wiktionary wordlists
-translate_other: prepare_other
-	$(foreach d,$(OTHER_DATASETS),rm -f ./data/$(d)/*.tra;)
-	$(foreach d,$(OTHER_DATASETS),python ./scripts/make_tr.py ./data/$(d)/*.wlh;)
+translate_other: disambiguate_other
+	@$(foreach d,$(OTHER_DATASETS),rm -f ./data/$(d)/*.tra;)
+	@$(foreach d,$(OTHER_DATASETS),python ./scripts/make_tr.py ./data/$(d)/*_dis.wlh;)
+	@rm -f ./data/$(CSSK)/*.tra
+	@python ./scripts/make_tr.py ./data/$(CSSK)/*_dis.wlh
+
+disambiguate_wikt: #process_wikt
+	@$(foreach d,$(wildcard data/*/wiktionary/*_dis.wlh),rm -f $(d);)
+	@$(foreach d,$(WIKT_LANGS),python ./scripts/disambiguate.py ./data/$(d)/wiktionary/*.wlh;)
+
+disambiguate_other: prepare_other
+	@$(foreach d,$(OTHER_DATASETS),rm -f ./data/$(d)/*_dis.wlh;)
+	@$(foreach d,$(OTHER_DATASETS),python ./scripts/disambiguate.py ./data/$(d)/*.wlh;)
+
+disambiguate_all: disambiguate_wikt disambiguate_other
 
 # extract data from compressed Wiktionary dump and prepare directory structure
 prepare_wikt:
-	unzip ./wikt_dump.zip -d .
-	$(foreach l,$(WIKT_LANGS),mkdir -p ./data/$(l)/wiktionary;)
+	@unzip ./wikt_dump.zip -d .
+	@$(foreach l,$(WIKT_LANGS),mkdir -p ./data/$(l)/wiktionary;)
 
 prepare_other:
-	python ./scripts/expand_weights.py ./data/cssk/cshyphen/cssk-all-weighted.wlhw
+	@python ./scripts/expand_weights.py ./data/cssk/cshyphen/cssk-all-weighted.wlhw
---
+++ scripts/disambiguate.py
@@ -0,0 +1,87 @@
+import re
+import argparse
+
+
+def hyph_indices(word: str, hyphenation_mark: str = "-"):
+    """
+    Find indices of hyphenation points within given word
+    :param word: string to be searched
+    :param hyphenation_mark: string used as hyphenation mark
+    """
+    return set([match.start() for match in re.finditer(hyphenation_mark, word)])
+
+
+def superset(first: set, second: set):
+    """
+    Check whether there is a superset-subset relation between two sets
+    :param first: set to be checked
+    :param second: set to be checked
+    :return: 0 if relation is present and first is the superset 1 if second is the superset, -1 otherwise
+    """
+    if first.issubset(second):
+        return 1
+    elif second.issubset(first):
+        return 0
+    return -1
+
+
+def disambiguate(file: str, hyphenation_mark: str = "-", outfile: str = ""):
+    """
+    Check and resolve inconsistencies in given dataset by joining or keeping words with non-unique hyphenations
+    :param file: path to word list to be processed
+    :param hyphenation_mark: string used as hyphenation mark
+    :param outfile: path to output file
+    :return: number of ambiguities (before, after) processing
+    """
+    words = dict()
+    found, disambiguated = 0, 0
+    with open(file) as wl:
+        for new in wl:
+            new = new.strip()
+            word = re.sub(hyphenation_mark, "", new)
+            if word not in words:
+                words[word] = [new]
+                continue
+            found += 1
+            new_ind = hyph_indices(new, hyphenation_mark)
+            resolved = False
+            for i, hyphenation in enumerate(words[word]):
+                hyph_ind = hyph_indices(hyphenation, hyphenation_mark)
+                superset_index = superset(new_ind, hyph_ind)
+                if superset_index != -1:
+                    if superset_index == 0:
+                        words[word][i] = new
+                    disambiguated += 1
+                    resolved = True
+                    break
+            if not resolved:
+                words[word].append(new)
+
+    if not outfile:
+        outfile = file+"_dis.wlh"
+
+    with open(outfile, "w") as out:
+        for word in sorted(words.keys()):
+            for hyphenation in sorted(words[word]):
+                out.write(hyphenation + "\n")
+
+    return found, found - disambiguated
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", type=str, help="File to be disambiguated")
+    parser.add_argument("-m", "--hyphmark", type=str, required=False, default="-", help="String used as hyphenation mark")
+    parser.add_argument("-o", "--outfile", type=str, required=False, default="", help="File to write output")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Enable more verbose printout")
+    args = parser.parse_args()
+
+    before, after = disambiguate(args.file, hyphenation_mark=args.hyphmark, outfile=args.outfile)
+    if args.verbose:
+        print(f"Disambiguated {args.file} into {args.outfile if args.outfile else args.file+'_dis.wlh'}, ambiguous hyphenations reduced from {before} to {after}")
+    else:
+        if "\\" in args.file:
+            split = args.file.split("\\")
+        else:
+            split = args.file.split("/")
+        print("" if len(split) < 3 else split[-3], "" if len(split) < 2 else split[-2], before, after)
