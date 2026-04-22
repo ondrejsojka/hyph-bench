aea85de756d13b086c9952d9233491ce071c259b
František Hůlka <frantah48@gmail.com>
2026-04-22 19:01:12 +0200

    fix: make it make sense

--- scripts/iterate_wordlist.py
+++ scripts/iterate_wordlist.py
@@ -8,9 +8,10 @@ TMP_PATH = "/tmp/hyph-bench"
 
 def implement_fixes(wordlist: str, fixes: str):
     subprocess.call(f"python -m scripts.replace_in_wordlist --wordlist {wordlist} --fixed {fixes} --output-dir {TMP_PATH}", shell=True)
-    subprocess.call(f"mv -f fixed.wlh {wordlist}", shell=True)
+    subprocess.call(f"mv -f {TMP_PATH}/fixed.wlh {wordlist}", shell=True)
 
 def compare_fixes(paths: list[str], result_dir: str):
+    print(THIN_SPACER)
     if len(paths) == 1:
         print("Only one file provided, no comparision will be done")
 
@@ -19,31 +20,68 @@ def compare_fixes(paths: list[str], result_dir: str):
     result_path = os.path.join(result_dir, "comparision.csv")
     subprocess.call(f"python -m scripts.compare_annotations {args} > {result_path}", shell=True)
     print(f"Comparision written to {result_path}")
+    print(THIN_SPACER)
 
 def run_optimizer(params: str):
     print("\nRunning optimizer")
     print(THIN_SPACER)
     subprocess.call("python -m scripts.optimize -b " + params, shell=True)
+    
+    print(THIN_SPACER)
+    print("Incorrectly hyphenated words found:")
+    subprocess.call(f"wc -l results/bad.txt", shell=True)
+    print(THIN_SPACER)
 
-def get_fix_files():
+def get_fix_files(last_order: list[str] = []) -> list[str]:
     print("\nOptimizer output a file to its results dir with words which were incorrectly hyphenated")
     print(THIN_SPACER)
     
-    response = input("Please re-hyphenate the words and input paths to the results or press enter to stop:\n")
-    paths = response.strip().split()
+    if last_order:
+        print("Last time provided in this order")
+    for i, file in enumerate(last_order):
+        print(f"{i + 1}. {file}")
 
-    if paths == []:
-        return [], -1
+    repeat = True
+    while repeat:
+        response = input("Please re-hyphenate the words and input paths to the results in the same order as last time or press enter to stop:\n")
+        paths = response.strip().split()
 
-    print("Following files provided:")
-    for i in range(len(paths)):
-        print(f"{i + 1}. {paths[i]}")
+        repeat = False
+        print("Following files provided:")
+        for i, file in enumerate(paths):
+            if not os.path.exists(file):
+                print(f"Path {file} does not exist. Please check it and re-enter the paths")
+                repeat = True
+            else:
+                print(f"{i + 1}. {file}")
 
-    path_i_str = input("Which of the files will be used to update the wordlist?\n")
-    path_i = int(path_i_str) - 1
+    return paths
 
-    print(f"Path {paths[path_i]} will be used")
-    return paths, path_i
+def create_iter_folder(n: int, output: str) -> str:
+    iteration_output = os.path.join(output, f"iter{n}")
+    os.makedirs(iteration_output)
+    return iteration_output
+
+def preprocess_params(params: list[str], wordlist: str, translate: str):
+    try:
+        wl_param_i = params.index("--wordlist") + 1
+    except ValueError:
+        params.append("--wordlist")
+        params.append(wordlist)
+        wl_param_i = len(params) - 1
+
+    try:
+        translate_param_i = params.index("--translate") + 1
+        params[translate_param_i] = translate
+    except ValueError:
+        params.append("--translate")
+        params.append(translate)
+
+    def modifer(wordlist_path):
+        params[wl_param_i] = wordlist_path
+
+    return modifer
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -52,8 +90,10 @@ def main():
         epilog=__doc__,
     )
 
-    parser.add_argument("--input", required=True, type=str,
+    parser.add_argument("--input-wordlist", required=True, type=str,
                         help="Hyphenated input wordlist (one word per line)")
+    parser.add_argument("--input-translate", required=True, type=str,
+                        help="Translate file for the wordlist")
     parser.add_argument("--output-dir", required=False, type=str, default="iter_results",
                         help="Output directory path")
     parser.add_argument("--optimizer-params", required=False, type=str, default="--objective bounded_bad --lang uk",
@@ -63,52 +103,72 @@ def main():
 
     args = parser.parse_args()
     output = args.output_dir
-    params = args.optimizer_params
-    wordlist = os.path.join(TMP_PATH, args.input)
+    params = args.optimizer_params.split()
+    wl_name = os.path.basename(args.input_wordlist)
+    wordlist = os.path.join(TMP_PATH, wl_name)
     iter_folders = os.path.join(output, "iter*")
+    prev_wordlists = os.path.join(output, "*.wlh")
+    add_wordlist_to_params = preprocess_params(params, wordlist, args.input_translate)
 
     os.makedirs(args.output_dir, exist_ok=True)
-    subprocess.call(f"cp {args.input} {wordlist}", shell=True)
+    subprocess.call(f"cp {args.input_wordlist} {wordlist}", shell=True)
     subprocess.call(f"rm -rf {iter_folders}", shell=True)
+    subprocess.call(f"rm {prev_wordlists}", shell=True)
 
     print(THICK_SPACER)
-    print(f"Iterating on {args.input}")
+    print(f"Iterating on {args.input_wordlist}")
     print(f"Results will be stored to {output}")
     print("Once there are few enough words in the bad.txt file output by the optimizer, we can stop")
     print(THICK_SPACER + "\n")
 
-    n = 1
-    while (True):
-        print(f"ITERATION {n}")
+    # inital run to get 500
+    n = 0
+    print(f"ITERATION {n}")
+    print(THIN_SPACER)
+
+    iteration_output = create_iter_folder(n, output)
+    run_optimizer(" ".join(params))
+
+    subprocess.call(f"cp results/bad.txt {iteration_output}", shell=True)
+
+    copied_wordlists = []
+    fix_files = get_fix_files()
+    compare_fixes(fix_files, iteration_output)
+
+    for i, fix in enumerate(fix_files):
+        copy_path = os.path.join(TMP_PATH, f"{i}.wlh")
+        copied_wordlists.append(copy_path)
+        subprocess.call(f"cp {wordlist} {copy_path}", shell=True)
+        implement_fixes(copy_path, fix)
+
+    while True:
+        n += 1
+        print(f"\nITERATION {n}")
         print(THIN_SPACER)
+        iteration_output = create_iter_folder(n, output)
 
-        iteration_output = os.path.join(output, f"iter{n}")
-        os.makedirs(iteration_output)
-        run_optimizer(params)
+        for i, wl in enumerate(copied_wordlists):
+            add_wordlist_to_params(wl)
+            run_optimizer(" ".join(params))
+            bad_file_name = os.path.join(iteration_output, f"{i}bad.txt")
+            subprocess.call(f"cp results/bad.txt {bad_file_name}", shell=True)
 
-        print(THIN_SPACER)
-        print("Incorrectly hyphenated words found:")
-        subprocess.call(f"wc -l results/bad.txt", shell=True)
-        print(THIN_SPACER)
-        
-        subprocess.call(f"cp results/bad.txt {iteration_output}", shell=True)
-
-        files, fix_i = get_fix_files()
-
-        if not files:
+        fixes = get_fix_files(fix_files)
+        if not fixes:
             break
 
-        compare_fixes(files, iteration_output)
-        implement_fixes(wordlist, files[fix_i])
-
-        n += 1
+        for i, file in enumerate(fixes):
+            implement_fixes(f"{TMP_PATH}/{i}.wlh", file, i)
 
     print(THICK_SPACER)
     print(f"Final iterations {n}")
     print(f"Storing result to {output}")
     print(THICK_SPACER)
 
-    subprocess.call(f"mv {wordlist} {output}", shell=True)
+    for i, file in enumerate(fix_files):
+        name = os.path.basename(file)
+        result_path = os.path.join(output, name + ".wlh")
+        subprocess.call(f"mv {TMP_PATH}/{i}.wlh {result_path}", shell=True)
 
 
 if __name__ == "__main__":
